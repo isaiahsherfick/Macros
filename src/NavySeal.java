1b07de4a8a2c0a0485eadca008413633f3bee5a1
isaiahsherfick <isaiahsherfick8@gmail.com>
2019-05-11 03:07:02 -0400

    asdf

--- src/NavySeal.java
+++ src/NavySeal.java
@@ -1,26 +1,19 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
-public class PastaTyper
+public class NavySeal extends PastaTyper
 {
-    protected String pasta;
-    protected Robot robot;
-
-    public PastaTyper(String pasta)
+    public NavySeal(String pasta)
     {
-        this.pasta = pasta;
-        try
-        {
-            this.robot = new Robot();
-        } catch (AWTException e)
-        {
-            e.printStackTrace();
-        }
+        super(pasta);
     }
-
+    @Override
     public void pastePasta()
     {
         int x = 0;
+        robot.delay(100);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyRelease(KeyEvent.VK_T);
         for (int i = 0; i < pasta.length(); i++)
         {
             char letter = pasta.charAt(i);
@@ -81,18 +74,16 @@ public class PastaTyper
                 robot.delay(500);
                 robot.keyPress(KeyEvent.VK_ENTER);
                 robot.keyRelease(KeyEvent.VK_ENTER);
+                robot.keyPress(KeyEvent.VK_T);
+                robot.keyRelease(KeyEvent.VK_T);
                 x = 0;
             }
         }
     }
 
-    public void setPasta(String pasta)
+    public static void main(String[] args)
     {
-        this.pasta = pasta;
-    }
-
-    public String getPasta()
-    {
-        return pasta;
+        PastaTyper ns = new PastaTyper("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
+        ns.pastePasta();
     }
 }
